0a1ef250e57afdb4b5bb373a305c06a4524c1ec9
James Fray <fe2sdbg@yahoo.com>
2017-02-22 16:24:16 +0330

    re-fetching all libs from 'https://cdnjs.cloudflare.com/ajax/libs/'

--- src/common.js
+++ src/common.js
@@ -117,15 +117,24 @@ app.on('update-badge', (tabId) => {
 chrome.tabs.onCreated.addListener((tab) => cache[tab.id] = []);
 // cleanup
 chrome.tabs.onRemoved.addListener((tabId) => delete cache[tabId]);
-// FAQs
-chrome.storage.local.get('version', (obj) => {
+
+// FAQs & Feedback
+chrome.storage.local.get({
+  'version': null,
+  'faqs': navigator.userAgent.toLowerCase().indexOf('firefox') === -1 ? true : false
+}, prefs => {
   let version = chrome.runtime.getManifest().version;
-  if (obj.version !== version) {
+
+  if (prefs.version ? (prefs.faqs && prefs.version !== version) : true) {
     chrome.storage.local.set({version}, () => {
       chrome.tabs.create({
-        url: 'http://add0n.com/local-cdn.html?version=' + version + '&type=' +
-          (obj.version ? ('upgrade&p=' + obj.version) : 'install')
+        url: 'http://add0n.com/local-cdn.html?version=' + version +
+          '&type=' + (prefs.version ? ('upgrade&p=' + prefs.version) : 'install')
       });
     });
   }
 });
+(function () {
+  let {name, version} = chrome.runtime.getManifest();
+  chrome.runtime.setUninstallURL('http://add0n.com/feedback.html?name=' + name + '&version=' + version);
+})();
---
+++ src/data/resources/check_md5.js
@@ -0,0 +1,83 @@
+'use strict';
+
+var crypto = require('crypto');
+var fs = require('fs');
+const https = require('https');
+
+var checksum = (str, algorithm, encoding) => crypto.createHash(algorithm || 'md5')
+  .update(str, 'utf8').digest(encoding || 'hex');
+
+var md5 = {
+  file: path => {
+    return new Promise(resolve => {
+      fs.readFile(path, (err, data) => {
+        resolve(err ? err : {
+          md5: checksum(data)
+        });
+      });
+    });
+  },
+  web: url => {
+    return new Promise(resolve => {
+      https.get(url, (res) => {
+        let data = [];
+        res.on('data', (chunk) => data.push(chunk));
+        res.on('end', () => {
+          if (res.statusCode === 200) {
+            let buffer = Buffer.concat(data);
+            resolve({
+              buffer,
+              md5: checksum(buffer)
+            });
+          }
+          else {
+            resolve({error: 'library not found!'});
+          }
+        });
+      }).on('error', (e) => resolve({error: e}));
+    });
+  }
+};
+
+function check (path) {
+  return Promise.all([
+    md5.file(path + '.dec'),
+    md5.web('https://ajax.googleapis.com/ajax/libs/' + path),
+    md5.web('https://cdnjs.cloudflare.com/ajax/libs/' + path),
+  ]).then(a => {
+    if (a[0].md5 === a[1].md5 || a[0].md5 === a[2].md5) {
+      console.log('OK', path, a[0].md5);
+    }
+    else {
+      console.log('\x1b[31m%s\x1b[0m: ', path);
+    }
+  });
+}
+
+function getFiles (dir, files_ = []) {
+  let files = fs.readdirSync(dir);
+  for (let i in files) {
+    let name = dir + '/' + files[i];
+    if (fs.statSync(name).isDirectory()) {
+      getFiles(name, files_);
+    }
+    else {
+      files_.push(name);
+    }
+  }
+  return files_;
+}
+
+// get all libs
+var files = getFiles('.').filter(n => n.endsWith('.dec')).map(n => n.replace('./', '').replace('.dec', ''));
+// check md5
+function next () {
+  let file = files.shift();
+  if (file) {
+    check(file).then(next);
+  }
+  else {
+    console.log('done!');
+  }
+}
+next();
